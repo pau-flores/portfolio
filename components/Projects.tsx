e9aa04de9370113fe7e4f3a4fe8921044776a69b
Paulina Flores <paufr.2822@gmail.com>
2024-07-27 02:39:31 -0700

    Updated projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,76 +1,98 @@
-import React from "react";
+import React, { useRef } from "react";
 import { motion, useInView } from "framer-motion";
-import Image from "next/image";
 
-const Projects = () => {
+interface ProjectCardProps {
+  title: string;
+  description: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ title, description }) => {
+  return (
+    <motion.div
+      className="bg-gray-900 p-6 rounded-lg shadow-lg mb-6"
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true, amount: 0.3 }}
+      whileHover={{ scale: 1.03, boxShadow: "0 10px 20px rgba(0,0,0,0.2)" }}
+    >
+      <h3 className="text-2xl font-bold mb-3 text-sky-400">{title}</h3>
+      <p className="text-gray-300">{description}</p>
+    </motion.div>
+  );
+};
+
+const Projects: React.FC = () => {
   const ref1 = React.useRef(null);
   const ref2 = React.useRef(null);
+  const ref3 = React.useRef(null);
+  const ref4 = React.useRef(null);
   const isInView1 = useInView(ref1, { once: true, amount: 0.3 });
   const isInView2 = useInView(ref2, { once: true, amount: 0.3 });
+  const isInView3 = useInView(ref2, { once: true, amount: 0.3 });
+  const isInView4 = useInView(ref2, { once: true, amount: 0.3 });
+
+  const projects = [
+    {
+      title: "Blake Vet Portal",
+      description:
+        "Developed a full-stack web application for veterinary practices. Implemented features such as patient management, appointment scheduling, note taking and more. Built with Laravel, React, and MySQL.",
+    },
+    {
+      title: "Vet Booking App",
+      description:
+        "A web application that allows users to book veterinary appointments for their pets. Built with Laravel, Livewire, Alpine.js and TailwindCSS.",
+    },
+    {
+      title: "Cat Food Finder",
+      description:
+        "Created a simple web application that allows users to search for cat food, based on dietary requirements and special needs. Built with React, Node.js, Express, and MongoDB.",
+    },
+    {
+      title: "The Incredible Journey",
+      description:
+        "Lost in a campsite, this is a single player, procedurally generated adventure game where you must find your way back home. Built with C# and Unity. Four person developer team.",
+    },
+  ];
 
   return (
-    <section className="relative bg-gray-900 text-white py-20 flex items-center justify-center overflow-hidden w-full">
-      <div className="mx-auto px-20 relative z-10">
-        <h2 className="text-4xl font-bold mb-12 text-center">My Projects</h2>
-
-        <motion.div
-          ref={ref1}
-          className="flex flex-col md:flex-row items-center mb-20"
-          initial={{ opacity: 0, x: -50 }}
-          animate={isInView1 ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }}
+    <section className="relative bg-slate-800 text-white py-40 overflow-hidden w-full">
+      <div className="container mx-auto px-20 relative z-10">
+        <motion.h3
+          ref={ref3}
+          className="text-xl font-bold uppercase mb-4 text-sky-300 text-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={isInView3 ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
         >
-          <div className="md:w-1/2 mb-6 md:mb-0">
-            <Image
-              src="/project1.jpg"
-              alt="Project 1"
-              width={500}
-              height={300}
-              className="rounded-lg shadow-lg"
-            />
-          </div>
-          <div className="md:w-1/2 md:pl-8">
-            <h3 className="text-2xl font-semibold mb-4">Project One</h3>
-            <p className="text-gray-300">
-              This is a description of Project One. It showcases my skills in
-              front-end development, utilizing React and Next.js to create a
-              responsive and interactive user interface. The project
-              demonstrates my ability to implement complex features while
-              maintaining clean, efficient code.
-            </p>
-          </div>
-        </motion.div>
-
-        <motion.div
+          Highlight Projects
+        </motion.h3>
+        <motion.h2
           ref={ref2}
-          className="flex flex-col md:flex-row-reverse items-center"
-          initial={{ opacity: 0, x: 50 }}
-          animate={isInView2 ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }}
+          className="text-4xl font-bold mb-6 text-center"
+          initial={{ opacity: 0, y: 20 }}
+          animate={isInView2 ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          What I've Been Working On
+        </motion.h2>
+        <motion.p
+          ref={ref4}
+          className="text-normal leading-relaxed text-center mb-10"
+          initial={{ opacity: 0, y: 20 }}
+          animate={isInView4 ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
           transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
         >
-          <div className="md:w-1/2 mb-6 md:mb-0">
-            <Image
-              src="/project2.jpg"
-              alt="Project 2"
-              width={500}
-              height={300}
-              className="rounded-lg shadow-lg"
-            />
-          </div>
-          <div className="md:w-1/2 md:pr-8">
-            <h3 className="text-2xl font-semibold mb-4">Project Two</h3>
-            <p className="text-gray-300">
-              Project Two highlights my back-end development skills. It involves
-              creating a robust API using Node.js and Express, integrating with
-              a MongoDB database. This project showcases my ability to design
-              efficient data models and implement secure authentication systems.
-            </p>
-          </div>
-        </motion.div>
-      </div>
-
-      <div className="absolute inset-0 z-0 flex items-end justify-center">
-        <div className="absolute h-[500px] w-full before:absolute before:h-[500px] before:w-full before:rounded-full before:bg-gradient-radial before:from-purple-600 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:bg-gradient-conic after:from-sky-900 after:via-purple-800 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-purple-700 before:dark:opacity-10 after:dark:opacity-40 sm:before:w-[780px] sm:after:w-[340px] before:lg:h-[560px]"></div>
+          Here you'll find a showcase of my latest work, from websites and web
+          applications, to games and more. Each project reflects my commitment
+          to quality and user-friendly design. Dive in to see how I bring ideas
+          to life!
+        </motion.p>
+        <div className="max-w-3xl mx-auto">
+          {projects.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </div>
       </div>
     </section>
   );
