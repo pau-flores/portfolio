697e30e835b82416f52940f86132a710c4d95eb0
Paulina Flores <paufr.2822@gmail.com>
2024-07-27 02:37:07 -0700

    Added scroll to about section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,34 +4,62 @@ import { motion, useInView } from "framer-motion";
 const Hero = () => {
   const ref1 = React.useRef(null);
   const ref2 = React.useRef(null);
+  const ref3 = React.useRef(null);
+  const ref4 = React.useRef(null);
   const isInView1 = useInView(ref1, { once: true, amount: 0.3 });
   const isInView2 = useInView(ref2, { once: true, amount: 0.3 });
+  const isInView3 = useInView(ref2, { once: true, amount: 0.3 });
+  const isInView4 = useInView(ref2, { once: true, amount: 0.3 });
+  const scrollToAbout = () => {
+    document.getElementById("skills").scrollIntoView({
+      behavior: "smooth",
+    });
+  };
 
   return (
-    <section className="relative bg-gray-900 text-white py-20 min-h-[400px] flex items-center justify-center overflow-hidden w-full">
+    <section className="relative bg-gray-900 text-white py-20 min-h-screen flex items-center justify-center overflow-hidden w-full">
       <div className="relative z-10 text-center mx-auto px-20">
+        <motion.h3
+          ref={ref3}
+          className="text-xl font-bold uppercase mb-4 text-sky-300"
+          initial={{ opacity: 0, y: 20 }}
+          animate={isInView3 ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          Hey there!
+        </motion.h3>
         <motion.h1
           ref={ref1}
-          className="text-5xl font-bold mb-4"
+          className="text-7xl font-bold mb-4"
           initial={{ opacity: 0, y: 20 }}
           animate={isInView1 ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
         >
-          Pau Flores
+          I'm Paulina Flores
         </motion.h1>
-        <motion.h3
+        <motion.h2
           ref={ref2}
           className="text-2xl font-light"
           initial={{ opacity: 0, y: 20 }}
-          animate={isInView2 ? { opacity: 1, y: 0 } : { opacity: 1, y: 0 }}
+          animate={isInView2 ? { opacity: 1, y: 0 } : { opacity: 1, y: -50 }}
           transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
         >
           Software Engineer & Full-Stack Developer
-        </motion.h3>
+        </motion.h2>
+
+        <motion.button
+          ref={ref4}
+          type="button"
+          initial={{ opacity: 0, y: 40 }}
+          transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
+          animate={isInView4 ? { opacity: 1, y: 0 } : { opacity: 1, y: -50 }}
+          className="mt-12 px-8 py-4 border-2 border-sky-300 text-white bg-transparent rounded-md flex items-center mx-auto"
+          onClick={scrollToAbout}
+        >
+          <span className="ml-2">Learn More About Me</span>
+        </motion.button>
       </div>
-      <div className="absolute inset-0 z-0 flex items-center justify-center">
-        <div className="absolute h-[300px] w-full before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-900 after:via-[#0141ff] after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]"></div>
-      </div>
+      <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-slate-800"></div>
     </section>
   );
 };
