0555db2bf3d331250fe50d8d56bcd92e6fa433b5
Paulina Flores <paufr.2822@gmail.com>
2024-08-09 04:07:34 -0700

    Installed primereact

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import { PrimeReactProvider } from "primereact/api";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "primereact/resources/primereact.css";
+import "primereact/resources/themes/lara-dark-cyan/theme.css";
 
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 
@@ -17,9 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <GoogleAnalytics />
-      <body className={inter.className}>{children}</body>
-    </html>
+    <PrimeReactProvider>
+      <html lang="en">
+        <GoogleAnalytics />
+        <body className={inter.className}>{children}</body>
+      </html>
+    </PrimeReactProvider>
   );
 }
